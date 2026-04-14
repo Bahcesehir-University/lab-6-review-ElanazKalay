3d17d36bb48a2c552b1ff6d9fca42ecfe1cb45b1
Elanaz Kalay <elanaz.kalay@bahcesehir.edu.tr>
2026-04-14 15:03:05 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -157,7 +157,7 @@ public:
     // Example: "Student(Ali, ID: 101, GPA: 3.5)"
     friend ostream& operator<<(ostream& os, const Student& s) {
         // YOUR CODE HERE
-        cout<<"student("<<s.name<<",ID;"<<s.id<<", GPA : "<<s.gpa<<")";
+        os<<"student("<<s.name<<",ID;"<<s.id<<", GPA : "<<s.gpa<<")";
         return os;
     }
 };
